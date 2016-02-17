01cc82d5d37c546e1568f68c288ba6b81d01df9b
AppleBabies <applebabiesmc@gmail.com>
2016-02-17 10:08:38 -0600

    Update Spinner.java

--- src/Spinner.java
+++ src/Spinner.java
@@ -16,6 +16,24 @@ public Spinner(Plugin p){
 
 public void show(Player p){
  p.openInventory(inv);
+ 
+ for(i=0; i<100;i++){
+  inv.getItem(11).setInventorySlot(inv.getItem(11)+1);
+  inv.getitem(12).setInventorySlot(inv.getItem(12)+1);
+  inv.getitem(13).setInventorySlot(inv.getItem(13)+1);
+  inv.getitem(14).setInventorySlot(inv.getItem(14)+1);
+  inv.getitem(15).setInventorySlot(inv.getItem(15)+1);
+  inv.getitem(16).setInventorySlot(inv.getItem(16)+1);
+  inv.getitem(17).setInventorySlot(inv.getItem(17)+1);
+  inv.getItem(18).setInventorySlot(1);
+  p.playSound(p.getLocation(),Sound.NOTE_STICKS,5, 1);
+ }
+ 
+}
+
+@EventHandler
+public void onInventoryClick(InventoryClickEvent e){
+ e.setCancelled(true);
 }
 
 }
